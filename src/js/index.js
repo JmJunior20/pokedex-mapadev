3adf3d879817f9033999d6333727b1ae9d458109
Jose Milton Junior <jmjuniormonteiro@gmail.com>
2022-03-14 11:10:18 -0300

    Adicionando id nas li

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// Criando duas variáveis para trabalhar com os elementos da tela
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    // Trabalhando com um evento de clique feito pelo usuário na listagem de pokémons
+    pokemon.addEventListener('click', () => {
+        // Remover a classe aberto só do cartão que está aberto
+
+    })
+})
